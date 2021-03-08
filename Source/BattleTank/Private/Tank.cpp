ae84ddf4d1de038b270df3a7a97393775a5ac112
Michael-Dao89 <michaeldong8989@gmail.com>
2021-03-08 16:50:02 +1000

    v1.0.2 Added main menu with graphic options

--- Source/BattleTank/Private/Tank.cpp
+++ Source/BattleTank/Private/Tank.cpp
@@ -9,11 +9,17 @@ float ATank::GetHealthPercent() const
 	return (float)CurrentHealth / (float)StartingHealth;
 }
 
+void ATank::AddHealth(float Amount)
+{
+	CurrentHealth += Amount;
+	return;
+}
+
 // Sets default values
 ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 }
 
 void ATank::BeginPlay()
